e40fe35f7831cbf25e78b21d660de228682977cb
kevencript <kevencript@gmail.com>
2023-01-07 17:04:04 -0300

    ffix

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -89,17 +89,17 @@ module "this_app_security_group" {
 # Subnet and Internet Gateway 
 resource "aws_subnet" "public" {
   count                   = 2
-  cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 2 + count.index)
   availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id                  = aws_vpc.default.id
+  vpc_id                  = var.vpc_id
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   count             = 2
-  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id            = aws_vpc.default.id
+  vpc_id            = var.vpc_id
 }
 
 resource "aws_internet_gateway" "gateway" {
