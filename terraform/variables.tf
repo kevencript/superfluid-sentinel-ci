cf8d4b55848a32932e5231ce5e7c5e6a558f5113
kevencript <kevencript@gmail.com>
2023-01-07 15:00:35 -0300

    fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,13 @@
 variable "vpc_cidr" {
   description = "CIDR range of VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/20"
 }
 
 # VPC variables
 variable "vpc_name" {
   description = "VPC name"
   type        = string
+  default     = "superfluid-ecs-vpc"
+
 }
