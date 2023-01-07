d3e5343b3f0277b7552dd9da47dda32988286c05
kevencript <kevencript@gmail.com>
2023-01-07 17:17:11 -0300

    ffix

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -102,44 +102,6 @@ resource "aws_subnet" "private" {
   vpc_id            = var.vpc_id
 }
 
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = var.vpc_id
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.default.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gateway.id
-}
-
-resource "aws_eip" "gateway" {
-  count      = 2
-  vpc        = true
-  depends_on = [aws_internet_gateway.gateway]
-}
-
-resource "aws_nat_gateway" "gateway" {
-  count         = 2
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.gateway.*.id, count.index)
-}
-
-resource "aws_route_table" "private" {
-  count  = 2
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  count          = 2
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
-}
-
 module "this_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 5.9"
