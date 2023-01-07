d7fef59023547b42a3414e1832ae0c3d15450290
kevencript <kevencript@gmail.com>
2023-01-07 18:36:18 -0300

    adding internet gageway

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -1,20 +1,20 @@
-output "this_cluster" {
-  value       = aws_ecs_cluster.this.name
+output "ecs_cluster_name" {
+  value       = aws_ecs_cluster.superfluid_ecs_cluster.name
   description = "Name of the ECS cluster."
 }
 
-output "this_app_security_group_id" {
-  value       = module.this_app_security_group.this_security_group_id
+output "superfluid_security_group_id" {
+  value       = module.superfluid_security_group.this_security_group_id
   description = "ID of the application Security Group."
 }
 
 output "this_lb_arn" {
-  value       = var.create_load_balancer ? module.this_alb[0].this_lb_arn : null
+  value       = var.create_load_balancer ? module.superfluid_alb[0].this_lb_arn : null
   description = "ARN of the AWS ALB (Application Load Balancer)."
 }
 
 output "this_lb_dns" {
-  value       = var.create_load_balancer ? module.this_alb[0].this_lb_dns_name : null
+  value       = var.create_load_balancer ? module.superfluid_alb[0].this_lb_dns_name : null
   description = "DNS name of the AWS ALB (Application Load Balancer)."
 }
 
