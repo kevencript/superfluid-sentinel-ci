a51f4541ad6ea2fa2828be36c068eae85b612b50
kevencript <kevencript@gmail.com>
2023-01-10 13:40:44 -0300

    adding structure back

--- terraform/modules/ecs-task/task-definition.tf
+++ terraform/modules/ecs-task/task-definition.tf
@@ -2,8 +2,8 @@ resource "aws_ecs_task_definition" "superfluid_sentinel" {
   family                   = "${var.name}-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 512
-  memory                   = 1000
+  cpu                      = 1024
+  memory                   = 2048
 
   execution_role_arn = module.ecs_task_execution_role.arn
 
@@ -15,8 +15,8 @@ resource "aws_ecs_task_definition" "superfluid_sentinel" {
 [
   {
     "image": "docker.io/gabrielobcosta/superfluid-sentinel:0.0.1",
-    "cpu": 512,
-    "memory": 1000,
+    "cpu": 1024,
+    "memory": 2048,
     "name": "superfluid-sentinel",
     "networkMode": "awsvpc",
     "essential": true,
