15033ac8fcd53624fc5deac6a80572c31d48777c
kevencript <kevencript@gmail.com>
2023-01-10 18:35:41 -0300

    adding structure back

--- terraform/modules/ecs-task/execution-role.tf
+++ terraform/modules/ecs-task/execution-role.tf
@@ -3,8 +3,29 @@ module "ecs_task_execution_role" {
 
   allow_ecr = true
   allow_create_log_groups    = true
-  cloudwatch_log_group_names = ["*"]
+  cloudwatch_log_group_names = [module.cloudwatch_log_group.name]
+
   name = format("app-%s-task-execution-role", var.name)
+
+  tags  = {
+    Application = var.name
+  }
+}
+
+module "cloudwatch_kms_key" {
+  source = "dod-iac/cloudwatch-kms-key/aws"
+
+  name = format("alias/%s-cloudwatch-logs", var.name)
+
+  tags  = {
+    Application = var.name
+  }
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name              = format("/aws/ecs/%s", var.name)
+  retention_in_days = 1 # expire logs after 1 day
+  kms_key_id        = module.cloudwatch_kms_key.aws_kms_key_arn
 
   tags  = {
     Application = var.name
