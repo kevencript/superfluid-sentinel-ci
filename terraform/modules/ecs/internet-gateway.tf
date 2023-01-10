8bdd14634ddb763e49fd3c1e179d4d852e2924ea
kevencript <kevencript@gmail.com>
2023-01-10 20:48:48 -0300

    CLEANING AND COMENTING

--- terraform/modules/ecs/internet-gateway.tf
+++ terraform/modules/ecs/internet-gateway.tf
@@ -1,39 +1,39 @@
-# ######################
-# ## Internet Gateway ##
-# resource "aws_internet_gateway" "gateway" {
-#   vpc_id = var.vpc_id
-# }
+######################
+## Internet Gateway ##
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = var.vpc_id
+}
 
-# resource "aws_route" "internet_access" {
-#   route_table_id         = var.vpc_main_route_table_id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.gateway.id
-# }
+resource "aws_route" "internet_access" {
+  route_table_id         = var.vpc_main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
 
-# resource "aws_eip" "gateway" {
-#   count      = 2
-#   vpc        = true
-#   depends_on = [aws_internet_gateway.gateway]
-# }
+resource "aws_eip" "gateway" {
+  count      = 2
+  vpc        = true
+  depends_on = [aws_internet_gateway.gateway]
+}
 
-# resource "aws_nat_gateway" "gateway" {
-#   count         = 2
-#   subnet_id     = element(aws_subnet.public.*.id, count.index)
-#   allocation_id = element(aws_eip.gateway.*.id, count.index)
-# }
+resource "aws_nat_gateway" "gateway" {
+  count         = 2
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.gateway.*.id, count.index)
+}
 
-# resource "aws_route_table" "private" {
-#   count  = 2
-#   vpc_id = var.vpc_id
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = var.vpc_id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
-#   }
-# }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
+  }
+}
 
-# resource "aws_route_table_association" "private" {
-#   count          = 2
-#   subnet_id      = element(aws_subnet.private.*.id, count.index)
-#   route_table_id = element(aws_route_table.private.*.id, count.index)
-# }
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
--- terraform/modules/ecs/loadbalancer.tf
+++ terraform/modules/ecs/loadbalancer.tf
@@ -1,77 +1,77 @@
-# ###################################
-# ##   Application Load Balancer   ##
-# ##                               ##
-# ##  OBS: Extra module: Sentinel  ##
-# ##       do not expose any port  ##
-# module "superfluid_alb" {
-#   source  = "terraform-aws-modules/alb/aws"
-#   version = "~> 5.9"
+###################################
+##   Application Load Balancer   ##
+##                               ##
+##  OBS: Extra module: Sentinel  ##
+##       do not expose any port  ##
+module "superfluid_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.9"
 
-#   count = var.create_load_balancer ? 1 : 0
+  count = var.create_load_balancer ? 1 : 0
 
-#   name = "${substr(random_string.cluster.keepers.name, 0, 12)}-${substr(random_string.cluster.keepers.environ, 0, 12)}-${random_string.cluster.id}"
+  name = "${substr(random_string.cluster.keepers.name, 0, 12)}-${substr(random_string.cluster.keepers.environ, 0, 12)}-${random_string.cluster.id}"
 
-#   load_balancer_type = "application"
+  load_balancer_type = "application"
 
-#   vpc_id          = var.vpc_id
-#   subnets         = aws_subnet.public.*.id
-#   security_groups = [module.superfluid_alb_security_group[0].this_security_group_id]
+  vpc_id          = var.vpc_id
+  subnets         = aws_subnet.public.*.id
+  security_groups = [module.superfluid_alb_security_group[0].this_security_group_id]
 
-#   target_groups = [{
-#     target_type      = "ip"
-#     backend_protocol = "HTTP"
-#     backend_port     = 80
-#   }]  
+  target_groups = [{
+    target_type      = "ip"
+    backend_protocol = "HTTP"
+    backend_port     = 80
+  }]  
 
-#   http_tcp_listeners = [{
-#     port     = 80
-#     protocol = "HTTP"
-#   }]
+  http_tcp_listeners = [{
+    port     = 80
+    protocol = "HTTP"
+  }]
 
-#   tags = merge(var.tags, local.tags)
-# }
+  tags = merge(var.tags, local.tags)
+}
 
-# #####################
-# ## Security Groups ##
-# module "superfluid_alb_security_group" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "~> 3.16"
+#####################
+## Security Groups ##
+module "superfluid_alb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.16"
 
-#   count = var.create_load_balancer ? 1 : 0
+  count = var.create_load_balancer ? 1 : 0
 
-#   name        = "${var.name}-${var.environ}-alb"
-#   description = "Access to the public facing Load Balancer"
-#   vpc_id      = var.vpc_id
+  name        = "${var.name}-${var.environ}-alb"
+  description = "Access to the public facing Load Balancer"
+  vpc_id      = var.vpc_id
 
-#   ingress_cidr_blocks = ["0.0.0.0/0"]
-#   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
 
-#   egress_cidr_blocks = ["0.0.0.0/0"]
-#   egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
 
-#   tags = merge(var.tags, local.tags)
-# }
+  tags = merge(var.tags, local.tags)
+}
 
-# module "superfluid_security_group" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "~> 3.16"
+module "superfluid_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.16"
 
-#   name   = "${var.name}-${var.environ}-app"
-#   vpc_id = var.vpc_id
+  name   = "${var.name}-${var.environ}-app"
+  vpc_id = var.vpc_id
 
-#   ingress_with_self = [{
-#     rule = "all-all"
-#   }]
+  ingress_with_self = [{
+    rule = "all-all"
+  }]
 
-#   computed_ingress_with_source_security_group_id = var.create_load_balancer ? [{
-#     rule                     = "http-80-tcp"
-#     source_security_group_id = module.superfluid_alb_security_group[0].this_security_group_id
-#   }] : []
+  computed_ingress_with_source_security_group_id = var.create_load_balancer ? [{
+    rule                     = "http-80-tcp"
+    source_security_group_id = module.superfluid_alb_security_group[0].this_security_group_id
+  }] : []
 
-#   number_of_computed_ingress_with_source_security_group_id = var.create_load_balancer ? 1 : 0
+  number_of_computed_ingress_with_source_security_group_id = var.create_load_balancer ? 1 : 0
 
-#   egress_cidr_blocks = ["0.0.0.0/0"]
-#   egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
 
-#   tags = merge(var.tags, local.tags)
-# }
+  tags = merge(var.tags, local.tags)
+}
