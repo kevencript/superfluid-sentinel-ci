a032042152c449499be2e4a82759fe7f51f18c47
kevencript <kevencript@gmail.com>
2023-01-24 11:15:49 -0300

    feat: adding Secret Manager Policy  resurce

--- terraform/modules/ecs-task/execution-role.tf
+++ terraform/modules/ecs-task/execution-role.tf
@@ -14,25 +14,26 @@ module "ecs_task_execution_role" {
   }
 }
 
+###########################
+## Secret Manager Policy ##
+resource "aws_iam_role_policy" "secrets_manager_policy_for_ecs" {
+  name = "${var.name}-ecs-secret-manager-policy"
+  role = module.ecs_task_execution_role.arn
 
-#############
-## Logging ##
-module "cloudwatch_kms_key" {
-  source = "dod-iac/cloudwatch-kms-key/aws"
-
-  name = format("alias/%s-cloudwatch-logs", var.name)
-
-  tags  = {
-    Application = var.name
-  }
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "secretsmanager:GetSecretValue"
+            ],
+            "Resource": "arn:aws:secretsmanager:*:*:*"
+        }
+    ]
+}
+EOF
 }
 
-resource "aws_cloudwatch_log_group" "main" {
-  name              = format("/aws/ecs/%s", var.name)
-  retention_in_days = 1 # expire logs after 1 day
-  kms_key_id        = module.cloudwatch_kms_key.aws_kms_key_arn
 
-  tags  = {
-    Application = var.name
-  }
-}
